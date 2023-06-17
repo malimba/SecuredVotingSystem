62cf81a0d3ee8fcef521e3cf948ec27426fc64f4
malimba <malimba@pop-os.localdomain>
2023-06-17 09:01:09 +0000

    added models

--- SecuredVotingSystem/votingapp/models.py
+++ SecuredVotingSystem/votingapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class VoteNominee(models.Model):
+    name = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='nominee_photos/')
+    position = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
